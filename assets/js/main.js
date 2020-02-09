f2f6dd0a3ae69dd807eb0e1a2f87f34e604bf5b4
Sidaartha Reddy <sidaarthareddy1998@gmail.com>
2020-02-09 14:24:18 +0530

    added back btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 var image =  document.getElementById("image");
 var btn1 =  document.getElementById("btn1");
 var btn0 =  document.getElementById("btn0");
+var btnb =  document.getElementById("btnb");
 
 function dangerStyle() {
 	document.body.style.backgroundColor = "#C53131";
@@ -8,6 +9,7 @@ function dangerStyle() {
 	document.getElementById("break").style.backgroundColor = "#EDAEAE";
 	document.getElementById("footer").style.borderColor = "#EDAEAE";
 	document.getElementById("footer-wrapper").style.color = "#EDAEAE";
+	document.getElementById("btn_back").style.visibility = "hidden";
 	a_tags = document.getElementsByTagName("a");
 	for (i = 0; i < a_tags.length; i++) {a_tags[i].style.color = "#EDAEAE";}
 	css = 'a:hover {color: #600B00 !important;}';
@@ -15,95 +17,104 @@ function dangerStyle() {
 	style.appendChild(document.createTextNode(css));
 	document.getElementsByTagName('head')[0].appendChild(style);
 	document.getElementsByClassName("heart")[0].style.color = "#600B00";
+	btn1.style.visibility = "hidden";
+	btn0.style.visibility = "hidden";
+	btnb.style.visibility = "hidden";
 }
 
 function nextQuestion(bool, qus) {
 	console.log(bool, qus)
-	if (qus == 1) {
+	if (qus == 0) {
+		image.src = "assets/img/1.png";
+		btn1.setAttribute("onClick", "nextQuestion(1,1);");
+		btn0.setAttribute("onClick", "nextQuestion(0,1);");
+		btnb.style.visibility = "hidden";
+	}else if (qus == 1) {
+		btnb.style.visibility = "visible";
 		if (bool == 1) {
 			image.src = "assets/img/2.png";
 			btn1.setAttribute("onClick", "nextQuestion(1,2);");
 			btn0.setAttribute("onClick", "nextQuestion(0,2);");
+			btnb.setAttribute("onClick", "nextQuestion(0,0);");
 		} else if (bool == 0) {
 			image.src = "assets/img/3.png";
 			btn1.setAttribute("onClick", "nextQuestion(1,3);");
 			btn0.setAttribute("onClick", "nextQuestion(0,3);");
+			btnb.setAttribute("onClick", "nextQuestion(0,0);");
 		}
 	} else if (qus == 2) {
+		btnb.style.visibility = "visible";
 		if (bool == 1) {
 			image.src = "assets/img/4.png";
 			btn1.setAttribute("onClick", "nextQuestion(1,4);");
 			btn0.setAttribute("onClick", "nextQuestion(0,4);");
+			btnb.setAttribute("onClick", "nextQuestion(1,1);");
 		} else if (bool == 0) {
 			image.src = "assets/img/F1.png";
-			btn1.style.visibility = "hidden";
-			btn0.style.visibility = "hidden";
 			dangerStyle()
 			document.getElementById("final_text").innerHTML='<span style="color: #EDAEAE;">You don’t truly need this item.</span>';
 		}
 	} else if (qus == 3) {
+		btnb.style.visibility = "visible";
 		if (bool == 1) {
 			image.src = "assets/img/F2.png";
-			btn1.style.visibility = "hidden";
-			btn0.style.visibility = "hidden";
 			dangerStyle()
 		} else if (bool == 0) {
 			image.src = "assets/img/5.png";
 			btn1.setAttribute("onClick", "nextQuestion(1,5);");
 			btn0.setAttribute("onClick", "nextQuestion(0,5);");
+			btnb.setAttribute("onClick", "nextQuestion(0,1);");
 		}
 	} else if (qus == 4) {
+		btnb.style.visibility = "visible";
 		if (bool == 1) {
 			image.src = "assets/img/6.png";
 			btn1.setAttribute("onClick", "nextQuestion(1,6);");
 			btn0.setAttribute("onClick", "nextQuestion(0,6);");
+			btnb.setAttribute("onClick", "nextQuestion(1,2);");
 		} else if (bool == 0) {
 			image.src = "assets/img/3.png";
 			btn1.setAttribute("onClick", "nextQuestion(1,3);");
 			btn0.setAttribute("onClick", "nextQuestion(0,3);");
+			btnb.setAttribute("onClick", "nextQuestion(1,2);");
 		}
 	} else if (qus == 5) {
+		btnb.style.visibility = "visible";
 		if (bool == 1) {
 			image.src = "assets/img/F3.png";
-			btn1.style.visibility = "hidden";
-			btn0.style.visibility = "hidden";
 			dangerStyle()
 		} else if (bool == 0) {
-			image.src = "assets/img/F5.png";
-			btn1.style.visibility = "hidden";
-			btn0.style.visibility = "hidden";
+			image.src = "assets/img/F4.png";
 			dangerStyle()
-			document.getElementById("final_text").innerHTML='<span style="color: #EDAEAE;">So you know you are getting a good deal.</span>';
+			document.getElementById("final_text").innerHTML='<span style="color: #EDAEAE;">If it’s something you really want.</span>';
 		}
 	} else if (qus == 6) {
+		btnb.style.visibility = "visible";
 		if (bool == 1) {
 			image.src = "assets/img/F1.png";
-			btn1.style.visibility = "hidden";
-			btn0.style.visibility = "hidden";
 			dangerStyle()
 			document.getElementById("final_text").innerHTML='<span style="color: #EDAEAE;">You don’t truly need this item.</span>';
 		} else if (bool == 0) {
 			image.src = "assets/img/9.png";
 			btn1.setAttribute("onClick", "nextQuestion(1,9);");
 			btn0.setAttribute("onClick", "nextQuestion(0,9);");
+			btnb.setAttribute("onClick", "nextQuestion(1,4);");
 		}
 	} else if (qus == 7) {
+		btnb.style.visibility = "visible";
 		if (bool == 1) {
 			image.src = "assets/img/8.png";
 			btn1.setAttribute("onClick", "nextQuestion(1,8);");
 			btn0.setAttribute("onClick", "nextQuestion(0,8);");
+			btnb.setAttribute("onClick", "nextQuestion(0,9);");
 		} else if (bool == 0) {
 			image.src = "assets/img/F5.png";
-			btn1.style.visibility = "hidden";
-			btn0.style.visibility = "hidden";
 			document.getElementById("final_text").innerHTML='<span style="color: #EDAEAE;">So you know you are getting a good deal.</span>';
 			dangerStyle()
 		}
 	} else if (qus == 8) {
 		if (bool == 1) {
 			image.src = "assets/img/F6.png";
-			btn1.style.visibility = "hidden";
-			btn0.style.visibility = "hidden";
 			document.body.style.backgroundColor = "#79BE87";
 			document.getElementById("major").style.color = "#B5FFC2";
 			document.getElementById("break").style.backgroundColor = "#B5FFC2";
@@ -116,24 +127,25 @@ function nextQuestion(bool, qus) {
 			style.appendChild(document.createTextNode(css));
 			document.getElementsByTagName('head')[0].appendChild(style);
 			document.getElementById("final_text").innerHTML='<span style="color: #B5FFC2;">Enjoy your purchase, smart spender!</span>';
-		} else if (bool == 0) {
-			image.src = "assets/img/F1.png";
 			btn1.style.visibility = "hidden";
 			btn0.style.visibility = "hidden";
+			btnb.style.visibility = "hidden";
+		} else if (bool == 0) {
+			image.src = "assets/img/F1.png";
 			dangerStyle()
 			document.getElementById("final_text").innerHTML='<span style="color: #EDAEAE;">Congrats! You prevented an impulse purchase and saved money!</span>';
 		}
 	} else if (qus == 9) {
+		btnb.style.visibility = "visible";
 		if (bool == 1) {
 			image.src = "assets/img/F4.png";
-			btn1.style.visibility = "hidden";
-			btn0.style.visibility = "hidden";
 			document.getElementById("final_text").innerHTML='<span style="color: #EDAEAE;">If it’s something you really want.</span>';
 			dangerStyle()
 		} else if (bool == 0) {
 			image.src = "assets/img/7.png";
 			btn1.setAttribute("onClick", "nextQuestion(1,7);");
 			btn0.setAttribute("onClick", "nextQuestion(0,7);");
+			btnb.setAttribute("onClick", "nextQuestion(0,6);");
 		}
 	}
 }
